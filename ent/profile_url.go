5dd4652b3e7e13c5de2db50f0a9dffb65faaab97
J.Dragon <j.dragon@chivincent.net>
2026-03-22 13:09:59 +0800

    fix: build url path with url.JoinPath

--- ent/profile_url.go
+++ ent/profile_url.go
@@ -1,20 +1,40 @@
 package ent
 
 import (
+	"log/slog"
+	"net/url"
 	"strings"
 )
 
-func (p *Profile) Url(url string, suffixes ...string) string {
+func (p *Profile) Url(baseUrl string, suffixes ...string) string {
 	if p.RemoteURL != "" {
 		return p.RemoteURL
 	}
 
-	return url + "/" + p.Username + strings.Join(suffixes, "")
+	res, err := url.JoinPath(baseUrl, p.Username, strings.Join(suffixes, ""))
+	if err != nil {
+		slog.Error("failed to join path",
+			slog.String("baseUrl", baseUrl),
+			slog.String("profile_username", p.Username),
+			slog.Any("suffixes", suffixes),
+		)
+	}
+
+	return res
 }
 
-func (p *Profile) Permalink(url string, suffixes ...string) string {
+func (p *Profile) Permalink(baseUrl string, suffixes ...string) string {
 	if p.RemoteURL != "" {
 		return p.RemoteURL
+	}
+
+	res, err := url.JoinPath(baseUrl, p.Username, strings.Join(suffixes, ""))
+	if err != nil {
+		slog.Error("failed to join path",
+			slog.String("baseUrl", baseUrl),
+			slog.String("profile_username", p.Username),
+			slog.Any("suffixes", suffixes),
+		)
 	}
 
 	return url + "/users/" + p.Username + strings.Join(suffixes, "")
